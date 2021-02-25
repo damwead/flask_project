8ff2ca9895e2b6a805ea810d3a63508904a071b7
Innokenty Mikhalevsky <innokentymikhalevsky@Star-Wonderer.local>
2021-02-25 21:31:59 +0300

    made login form with simple validators

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from forms import LoginForm
 from config import Config
 
@@ -9,7 +9,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
+@app.route("/index", methods=["GET", "POST"])
 def index():
     user = {'username': 'Kenny'}
     posts = [
@@ -23,3 +24,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route("/login",  methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me{}".format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sing In", form=form)
+
+
